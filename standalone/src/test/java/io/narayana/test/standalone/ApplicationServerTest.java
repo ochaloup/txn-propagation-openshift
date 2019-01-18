e9d8d17b48bd606409b93819ac98fd5ca15a6afd
Ondra Chaloupka <ochaloup@redhat.com>
2019-01-18 21:37:22 +0100

    tests for structure and downloading jboss dist

--- standalone/src/test/java/io/narayana/test/standalone/ApplicationServerTest.java
+++ standalone/src/test/java/io/narayana/test/standalone/ApplicationServerTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.io.File;
 import java.nio.file.Path;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +17,8 @@ import io.narayana.test.utils.FileUtils;
 
 @ExtendWith(TempDirectory.class)
 public class ApplicationServerTest {
+    private static final String CONFIG_CONTENT =
+            "<?xml version='1.0' encoding='UTF-8'?><server xmlns=\"urn:jboss:domain:10.0\"></server>";
 
     @BeforeEach
     public void prepareServerStructure(@TempDir Path tempDir) {
@@ -23,26 +26,53 @@ public class ApplicationServerTest {
         Path originStandalone = originServer.resolve("standalone");
         File confDir = originStandalone.resolve("configuration").toFile();
         confDir.mkdirs();
-        FileUtils.writeFile(new File(confDir, "standalone.xml"), "<?xml version='1.0' encoding='UTF-8'?><server xmlns=\"urn:jboss:domain:10.0\"></server>");
+        FileUtils.writeFile(new File(confDir, "standalone.xml"), CONFIG_CONTENT);
+        FileUtils.writeFile(new File(confDir, "standalone-full.xml"), CONFIG_CONTENT);
 
-        originStandalone.resolve("standalone" + File.separator + "data").toFile().mkdirs();
-        originStandalone.resolve("standalone" + File.separator + "tmp").toFile().mkdirs();
-        originStandalone.resolve("standalone" + File.separator + "log").toFile().mkdirs();
-        originStandalone.resolve("standalone" + File.separator + "content").toFile().mkdirs();
+        originStandalone.resolve("data").toFile().mkdirs();
+        originStandalone.resolve("tmp").toFile().mkdirs();
+        originStandalone.resolve("log").toFile().mkdirs();
+        originStandalone.resolve("content").toFile().mkdirs();
 
         tempDir.resolve("target").toFile().mkdirs();
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        TestConfig.clear();
     }
 
     @Test
     public void standaloneServer(@TempDir Path tempDir) {
         Path originDir = tempDir.resolve("origin");
+        Path targetDir = tempDir.resolve("target");
         TestConfig.put("jboss.home", originDir.toFile().getAbsolutePath());
-        TestConfig.put("jboss.target.path", tempDir.resolve("target").toFile().getAbsolutePath());
+        TestConfig.put("jboss.target.path", targetDir.toFile().getAbsolutePath());
 
         assertThat(originDir.resolve("standalone").resolve("configuration")).exists();
 
-        ApplicationServer s = new ApplicationServer("default")
-                .prepare();
-        // System.out.println(s.isStarted() ? "YES" : "NO");
+        new ApplicationServer("default").prepare();
+
+        Path targetStandalone = targetDir.resolve("standalone");
+        assertThat(targetStandalone.resolve("configuration")).exists();
+        assertThat(targetStandalone.resolve("configuration").resolve("standalone.xml")).exists();
+        assertThat(targetStandalone.resolve("configuration").resolve("standalone-full.xml")).doesNotExist();
+        assertThat(targetStandalone.resolve("tmp")).exists();
+        assertThat(targetStandalone.resolve("log")).exists();
+        assertThat(targetStandalone.resolve("content")).exists();
+    }
+
+    @Test
+    public void configDefined(@TempDir Path tempDir) {
+        Path originDir = tempDir.resolve("origin");
+        Path targetDir = tempDir.resolve("target");
+        TestConfig.put("jboss.home", originDir.toFile().getAbsolutePath());
+        TestConfig.put("jboss.target.path", targetDir.toFile().getAbsolutePath());
+        TestConfig.put("jbosss.config", "standalone-full.xml");
+
+        new ApplicationServer("default").prepare();
+
+        Path targetStandalone = targetDir.resolve("standalone");
+        assertThat(targetStandalone.resolve("configuration").resolve("standalone-full.xml")).doesNotExist();
     }
 }
